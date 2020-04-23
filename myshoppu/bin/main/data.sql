156e240023cb6b98bfa097fe189ee5ff2b577ce7
Youness Ayyada <50589332+Youness-Ayyada@users.noreply.github.com>
2020-04-23 18:38:12 +0000

    v3

--- myshoppu/bin/main/data.sql
+++ myshoppu/bin/main/data.sql
@@ -2,19 +2,19 @@ insert into public.user(id, username, password, first_name, last_name, email_add
     values (0, 'root', '$2a$10$XpLVTvFHekyqAIvKkPIwuuKXiL/3RNswPldw85T6MtDFQcxo2OUve', 'Root', 'Root', 'root@local.host', 'root address', true);
 insert into user_type(id, username, user_type, active) values (0, 'root', 'ROLE_SUPERADMIN', true);
 
-insert into shop(id, name, active) values (0, 'OPPO', true);
-insert into shop(id, name, active) values (1, 'SAMSUNG', true);
-insert into shop(id, name, active) values (2, 'APPLE', true);
+insert into shop(id, name, active) values (0, 'MohammedShop', true);
+insert into shop(id, name, active) values (1, 'Salih coordonnier', true);
+insert into shop(id, name, active) values (2, 'Confiture Mehdi', true);
 
 insert into category(id, name, active) values (0, 'Root', true);
-insert into category(id, name, parent_category_id, active) values (1, 'Electronics', 0, true);
-insert into category(id, name, parent_category_id, active) values (2, 'Phones', 1, true);
-insert into category(id, name, parent_category_id, active) values (3, 'Workstaions', 1, true);
-insert into category(id, name, parent_category_id, active) values (4, 'Laptops', 1, true);
-insert into category(id, name, parent_category_id, active) values (5, 'Bikes', 0, true);
-insert into category(id, name, parent_category_id, active) values (6, 'Cruiser Bikes', 5, true);
-insert into category(id, name, parent_category_id, active) values (7, 'Sport Bikes', 5, true);
-insert into category(id, name, parent_category_id, active) values (8, 'Touring Bikes', 5, true);
+insert into category(id, name, parent_category_id, active) values (1, 'Kitchen', 0, true);
+insert into category(id, name, parent_category_id, active) values (2, 'Food', 1, true);
+insert into category(id, name, parent_category_id, active) values (3, 'Ustensiles', 1, true);
+insert into category(id, name, parent_category_id, active) values (4, 'Clothes', 0, true);
+insert into category(id, name, parent_category_id, active) values (5, 'Shoes', 4, true);
+insert into category(id, name, parent_category_id, active) values (6, 'Home', 0, true);
+insert into category(id, name, parent_category_id, active) values (7, 'Carpet', 6, true);
+insert into category(id, name, parent_category_id, active) values (8, 'Bikes', 0, true);
 insert into category(id, name, parent_category_id, active) values (9, 'Dirt Bikes', 5, true);
 insert into category(id, name, parent_category_id, active) values (10, 'Books', 0, true);
 insert into category(id, name, parent_category_id, active) values (11, 'Literature', 10, true);
@@ -22,10 +22,10 @@ insert into category(id, name, parent_category_id, active) values (12, 'Fiction'
 insert into category(id, name, parent_category_id, active) values (13, 'Science', 10, true);
 
 insert into product(id, name, description, image, barcode, shop_id, category_id, price, quantity, weight, discount, active) 
-    values (0, 'F1 Plus', 'The 16MP front camera sensor was exclusively designed for OPPO and the F1 Plus with the ability to pick up four times the incoming light as other smartphone cameras.', 'f1plus.jpg', '123456', 0, 2, 450, 5, 250, 0, true);
+    values (0, 'tajine', 'traditional tajine made of clay with a moroccan design', 'oldtajine.jpg', '123456', 0, 3, 45, 5, 250, 0, true);
 insert into product(id, name, description, image, barcode, shop_id, category_id, price, quantity, weight, discount, active)
-    values (1, 'S10', 'Triple rear camera setup: 16MP with f2.2 aperture ultra wide + 12MP with f1.5 and f2.4 aperture wide + 12MP f2.4 tele.', 's10.jpg', '654321', 1, 2, 900, 5, 200, 0, true);
+    values (1, 'Traditional sandals', ' leathermade red sandals with a moroccan style', 'redsandals.jpg', '654321', 1, 5, 900, 5, 200, 0, true);
 insert into product(id, name, description, image, barcode, shop_id, category_id, price, quantity, weight, discount, active) 
-    values (2, 'iPhone X', 'A11 Bionic Chip with Hexa-Core Processor, iOS, Embedded M11 Motion CoProcessor.', 'ix.jpg', '743987', 2, 2, 1100, 5, 200, 0, true);
+    values (2, 'Strwberry jam', 'homemade strawberry jam', 'strawberryjam.jpg', '743987', 2, 2, 1100, 5, 200, 0, true);
 insert into product(id, name, description, image, barcode, shop_id, category_id, price, quantity, weight, discount, active) 
-    values (3, 'iPhone 11', 'A11 Bionic Chip with Hexa-Core Processor, iOS, Embedded M11 Motion CoProcessor.', 'ix.jpg', '769654', 2, 2, 1300, 5, 200, 0, true);
+    values (3, 'Red Carpet', 'Red traditional Carpet.', 'redcarpet.jpg', '769654', 0, 7, 1300, 5, 200, 0, true);
